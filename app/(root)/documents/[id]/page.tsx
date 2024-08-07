f94a577dd157491b0fc293f95885ec2c8ba1ef6d
Leo Cheung <lappangcheung@gmail.com>
2024-08-06 22:29:46 -0400

    Added the sticky comments

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -3,6 +3,7 @@ import CollaborativeRoom from "@/components/CollaborativeRoom";
 import {currentUser} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
 import {getDocument} from "@/lib/actions/room.actions";
+import {getClerkUsers} from "@/lib/actions/user.actions";
 
 
 const Document = async ({params: { id }}: SearchParamProps) => {
@@ -16,13 +17,26 @@ const Document = async ({params: { id }}: SearchParamProps) => {
 
   if (!room) redirect('/')
 
-  // TODO: Access the permission level of the users
+  const userIds = Object.keys(room.usersAccesses)
+  const users = await getClerkUsers({ userIds })
+
+  const userData = users.map((user:User) => ({
+    ...user,
+    userType: room.usersAccesses[user.email]?.includes('room:write')
+      ? 'editor'
+      : 'viewer',
+  }))
+
+  const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]
+    ?.includes('room:write') ? 'editor' : 'viewer'
 
   return (
     <main className="flex w-full flex-col items-center">
       <CollaborativeRoom
         roomId={id}
         roomMetadata={room.metadata}
+        users={userData}
+        currentUserType={currentUserType}
       />
     </main>
   )
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -11,9 +11,7 @@ import Image from "next/image";
 import {updateDocument} from "@/lib/actions/room.actions";
 import Loader from "@/components/Loader";
 
-const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps ) => {
-
-  const currentUserType = 'editor'
+const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps ) => {
 
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
   const [editing, setEditing] = useState(false)
@@ -118,7 +116,8 @@ const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps ) =>
               </SignedIn>
             </div>
           </Header>
-          <Editor />
+          <Editor roomId={roomId}
+                  currentUserType={currentUserType}/>
         </div>
       </ClientSideSuspense>
     </RoomProvider>
