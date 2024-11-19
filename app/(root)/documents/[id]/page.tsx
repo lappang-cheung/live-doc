cee82b60c5480b170520803a53cb184485e799af
Leo Cheung <lappangcheung@gmail.com>
2024-11-18 22:13:24 -0500

    Fix the page issue

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -22,7 +22,7 @@ const Document = async ({params: { id }}: SearchParamProps) => {
 
   const userData = users.map((user:User) => ({
     ...user,
-    userType: room.usersAccesses[user.email]?.includes('room:write')
+    userType: room.usersAccesses[user?.email]?.includes('room:write')
       ? 'editor'
       : 'viewer',
   }))
